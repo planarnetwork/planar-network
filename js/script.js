50992f40a2305786081239da08a9f5880e384517
perryharlock <perry@phwebs.co.uk>
2018-03-07 11:15:16 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    // Variables and Cached Selectors
+    var example         = 0,
+        $body           = $('body'),
+        $example        = $('example');
+
+    $body.removeClass('no-js');
+
+    // Functions
+    function example (item) {
+        
+    }
+
+    $example.on('click', function() {
+        example(this);
+    });
+});
